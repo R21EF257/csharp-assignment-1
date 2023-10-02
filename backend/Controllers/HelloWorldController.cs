8cd7a8c51db74b4186129c9df6826f8e88a11e47
R21EF257 <146724627+R21EF257@users.noreply.github.com>
2023-10-02 17:11:10 +0000

    feat: add dotnet webapi files

---
+++ backend/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello World";
+        }
+    }
+}
