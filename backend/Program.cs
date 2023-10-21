e6754232ce1b347d86df34909dd7778a7fb86640
R21EF257 <146724627+R21EF257@users.noreply.github.com>
2023-10-21 20:03:00 +0000

    feat: add main game (local integration)

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,11 +6,25 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            // Replace with your client's address, or use '*' to allow any,
+            // but be aware that allowing any can lead to security risks.
+            builder.WithOrigins("*") // Replace with your frontend's actual origin
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,11 +34,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+// Apply the CORS policy globally
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
